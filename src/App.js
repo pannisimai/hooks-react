200ff1a0b969329f8580d78dc1be088d0bea2f7f
Panni <simaianna3@gmail.com>
2019-06-12 09:44:10 +0200

    contextapi

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import ProductListClass from "./ProductListClass";
-import ProductListHook from "./ProductListHook";
+import Context from "./components/ContextApi";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <ProductListHook />
+        <Context />
       </header>
     </div>
   );
